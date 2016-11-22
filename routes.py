310fc6ebac76a3cf13b7b5931ae0992e0543279e
Hanmei Tang <hat003@eng.ucsd.edu>
2016-11-21 17:20:01 -0800

    connect to mongodb

--- routes.py
+++ routes.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+mongo = PyMongo(app)
+
 
 @app.route("/")
 def index():
-  return render_template("index.html")
+    return render_template("index.html")
+
+
+@app.route('/')
+def home_page():
+    online_users = mongo.db.users.find({'online': True})
+    return render_template('index.html',
+        online_users=online_users)
+
 
 @app.route("/about")
 def about():
-  return render_template("about.html")
+    return render_template("about.html")
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
