9bbe1e9d94e219081d8d62a89496222db8e2b739
Hanmei Tang <hat003@eng.ucsd.edu>
2016-11-25 20:15:38 -0800

    minor update

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
+from pymongo import MongoClient
 
 app = Flask(__name__)
 mongo = PyMongo(app)
@@ -10,10 +11,10 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/')
+@app.route('/homepage')
 def home_page():
-    online_users = mongo.db.users.find({'online': True})
-    return render_template('index.html',
+    online_users = mongo.db.neb.find({'neb_id': 1})
+    return render_template('homepage.html',
         online_users=online_users)
 
 
